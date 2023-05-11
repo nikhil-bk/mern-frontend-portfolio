0467eec3dae8bb08a93ed53fa6ae38895814921e
Nikhil bk <nikhilbk9148@gmail.com>
2023-05-11 22:34:28 +0530

    Final code changes

--- src/components/Homepages/Header.js
+++ src/components/Homepages/Header.js
@@ -129,7 +129,7 @@ const Header = () => {
              </div>
 
              <div className="cv">
-                 <span><b>Cv:</b> <a href="https://drive.google.com/file/d/1TmJYUL3hwcfrtK80ptwELk9ETMuz8b7r/view?usp=share_link" target="_blank" rel="noreferrer"><i className="fas fa-file-pdf"></i></a></span>
+                 <span><b>Cv:</b> <a href="https://drive.google.com/file/d/1uqRqyMGd95JjZY-AXnQjv1qLSOs2FPl3/view?usp=sharing" target="_blank" rel="noreferrer"><i className="fas fa-file-pdf"></i></a></span>
              </div>
 
               </div>
