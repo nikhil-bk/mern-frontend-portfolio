b3de3bc3b112ab2b3deed022906be2f749e4c4ee
nikhil-bk <nikhilbk9148@gmail.com>
2023-10-12 09:25:45 +0530

    added cv new link

--- src/components/Homepages/Header.js
+++ src/components/Homepages/Header.js
@@ -125,11 +125,10 @@ const Header = () => {
                      />
                  </h1>
 
-                 
              </div>
 
              <div className="cv">
-                 <span><b>Cv:</b> <a href="https://drive.google.com/file/d/1uqRqyMGd95JjZY-AXnQjv1qLSOs2FPl3/view?usp=sharing" target="_blank" rel="noreferrer"><i className="fas fa-file-pdf"></i></a></span>
+                 <span><b>Cv:</b> <a href="https://drive.google.com/file/d/15S3PR462jo7S206SbK5TCDifo9J5XXNu/view?usp=drive_link" target="_blank" rel="noreferrer"><i className="fas fa-file-pdf"></i></a></span>
              </div>
 
               </div>
