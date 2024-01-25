95d4054a56cdbdbbc158a7a63ba5c0d68e2213d7
nikhil-bk <nikhilbk9148@gmail.com>
2024-01-25 08:47:53 +0530

    CV linked added

--- src/components/Homepages/Header.js
+++ src/components/Homepages/Header.js
@@ -128,7 +128,7 @@ const Header = () => {
              </div>
 
              <div className="cv">
-                 <span><b>Cv:</b> <a href="https://drive.google.com/file/d/15S3PR462jo7S206SbK5TCDifo9J5XXNu/view?usp=drive_link" target="_blank" rel="noreferrer"><i className="fas fa-file-pdf"></i></a></span>
+                 <span><b>CV:</b> <a href="https://drive.google.com/file/d/1_jCqllfxT2I9jxcJuVL87CXFLdYURCh0/view?usp=drive_link" target="_blank" rel="noreferrer"><i className="fas fa-file-pdf"></i></a></span>
              </div>
 
               </div>
